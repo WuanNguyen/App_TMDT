d5f2d8319034d11ef0ea54965ca32a78e581c0fd
LeNgocBaThong <ngunguoi0909@gmail.com>
2024-01-08 20:53:06 +0700

    Update product_detail_page.dart

--- lib/screen/product_detail_page.dart
+++ lib/screen/product_detail_page.dart
@@ -80,24 +80,24 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
                         ),
                         Container(
                           width: MediaQuery.of(context).size.width,
-                          child: const Padding(
-                            padding: EdgeInsets.all(10),
+                          child: Padding(
+                            padding: EdgeInsets.all(0.1),
                             child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Column(
-                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                       Text('Là một sản phẩm thời trang đang được yêu thích bởi giới trẻ hiện nay.',softWrap: true,style: TextStyle(color: Colors.black,fontSize: 17,fontWeight: FontWeight.normal,decoration: TextDecoration.none),),
-                                  ],
-                                )
+                                Container(
+                                  width: 320,
+                                  padding: EdgeInsets.all(0.1),
+                                  child: const Text('Là một sản phẩm thời trang đang được yêu thích bởi giới trẻ hiện nay.',softWrap: true,style: TextStyle(color: Colors.black,fontSize: 17,decoration: TextDecoration.none,fontWeight: FontWeight.normal),),
+                                ),
                               ],
                             ),
                           ),
                         ),
                         const SizedBox(height: 15),
                         const Padding(
-                          padding: EdgeInsets.all(10),
+                          padding: EdgeInsets.all(5),
                           child: Row(
                             children: [
                               Text("Size",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 20,decoration: TextDecoration.none),),
@@ -105,17 +105,19 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
                           )
                         ),
                         Container(
-                          width: MediaQuery.of(context).size.width,
                           child: Padding(
-                            padding: EdgeInsets.all(10),
+                            padding: EdgeInsets.all(0.1),
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.spaceAround,
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                ElevatedButton(
+                                Container(
+                                  width: 90,
+                                  padding: EdgeInsets.all(0.1),
+                                  child: ElevatedButton(
                                   onPressed: (){},
                                   style: ElevatedButton.styleFrom(
-                                    minimumSize: const Size(100, 50),
+                                    minimumSize: const Size(90, 50),
                                     backgroundColor: Colors.transparent,
                                     shape: const RoundedRectangleBorder(
                                       borderRadius: BorderRadius.all(
@@ -125,11 +127,14 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
                                   ),
                                   child: Text("39",style: TextStyle(color: Colors.black))
                                 ),
-                                Spacer(),
-                                ElevatedButton(
+                                ),
+                                Container(
+                                  width: 90,
+                                  padding: EdgeInsets.all(0.1),
+                                  child: ElevatedButton(
                                   onPressed: (){},
                                   style: ElevatedButton.styleFrom(
-                                    minimumSize: const Size(100, 50),
+                                    minimumSize: const Size(90, 50),
                                     backgroundColor: Colors.transparent,
                                     shape: const RoundedRectangleBorder(
                                       borderRadius: BorderRadius.all(
@@ -139,11 +144,14 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
                                   ),
                                   child: Text("40",style: TextStyle(color: Colors.black))
                                 ),
-                                Spacer(),
-                                ElevatedButton(
+                                ),
+                                Container(
+                                  width: 90,
+                                  padding: EdgeInsets.all(0.1),
+                                  child: ElevatedButton(
                                   onPressed: (){},
                                   style: ElevatedButton.styleFrom(
-                                    minimumSize: const Size(100, 50),
+                                    minimumSize: const Size(90, 50),
                                     backgroundColor: Colors.transparent,
                                     shape: const RoundedRectangleBorder(
                                       borderRadius: BorderRadius.all(
@@ -153,22 +161,25 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
                                   ),
                                   child: Text("41",style: TextStyle(color: Colors.black),)
                                 ),
+                                ),
                               ],
                             ),
                           ),
                         ),
                         Container(
-                          width: MediaQuery.of(context).size.width,
                           child: Padding(
-                            padding: EdgeInsets.all(10),
+                            padding: EdgeInsets.all(5),
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.spaceAround,
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                ElevatedButton(
+                                Container(
+                                  width: 90,
+                                  padding: EdgeInsets.all(0.1),
+                                  child: ElevatedButton(
                                   onPressed: (){},
                                   style: ElevatedButton.styleFrom(
-                                    minimumSize: const Size(100, 50),
+                                    minimumSize: const Size(90, 50),
                                     backgroundColor: Colors.transparent,
                                     shape: const RoundedRectangleBorder(
                                       borderRadius: BorderRadius.all(
@@ -178,10 +189,14 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
                                   ),
                                   child: Text("42",style: TextStyle(color: Colors.black))
                                   ),
-                                ElevatedButton(
+                                ),
+                                Container(
+                                  width: 90,
+                                  padding: EdgeInsets.all(0.1),
+                                  child: ElevatedButton(
                                   onPressed: (){},
                                   style: ElevatedButton.styleFrom(
-                                    minimumSize: const Size(100, 50),
+                                    minimumSize: const Size(90, 50),
                                     backgroundColor: Colors.transparent,
                                     shape: const RoundedRectangleBorder(
                                       borderRadius: BorderRadius.all(
@@ -191,24 +206,29 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
                                   ),
                                   child: Text("43",style: TextStyle(color: Colors.black))
                                   ),
+                                ),
                               ],
                             ),
                           ),
                         ),
                         const SizedBox(height: 15),
                         const Padding(
-                          padding: EdgeInsets.all(10),
+                          padding: EdgeInsets.all(5),
                           child: Text("Chính sách",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 20,decoration: TextDecoration.none),),
                         ),
                         Container(
                           width: MediaQuery.of(context).size.width,
-                          child: const Padding(
-                            padding: EdgeInsets.all(10),
+                          child: Padding(
+                            padding: EdgeInsets.all(0.1),
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.spaceAround,
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text('Chúng tôi cam kết hàng đúng như mô tả có thể trả hàng trong vòng 7 ngày.',softWrap: true,style: TextStyle(color: Colors.black,fontSize: 17,decoration: TextDecoration.none,fontWeight: FontWeight.normal),),
+                                Container(
+                                  width: 320,
+                                  padding: EdgeInsets.all(0.1),
+                                  child: const Text('Chúng tôi cam kết hàng đúng như mô tả có thể trả hàng trong vòng 7 ngày.',softWrap: true,style: TextStyle(color: Colors.black,fontSize: 17,decoration: TextDecoration.none,fontWeight: FontWeight.normal),),
+                                ),
                               ],
                             ),
                           ),
