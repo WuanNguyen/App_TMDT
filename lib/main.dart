5bd44f51d122fe43729cc4595764c6fd464e0913
Viz <nghiemvi1234567890@gmail.com>
2024-01-16 11:18:07 +0700

    homepage undo phần comment khúc appbar, còn lại đã copy theo main branch

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
+import 'package:doan_tmdt/firebase_options.dart';
 import 'package:doan_tmdt/model/bottom_appar.dart';
-import 'package:doan_tmdt/model/search_product.dart';
-import 'package:doan_tmdt/screen/forgot_password_page.dart';
-import 'package:doan_tmdt/screen/home_page.dart';
-import 'package:doan_tmdt/screen/login_page.dart';
-import 'package:doan_tmdt/screen/register_page.dart';
-import 'package:doan_tmdt/screen/search_page.dart';
+import 'package:doan_tmdt/screen/cart/cart_page.dart';
+import 'package:doan_tmdt/screen/home/product_detail_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-void main() {
+import 'package:doan_tmdt/screen/login/login_page.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,8 +23,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      debugShowCheckedModeBanner: false,
-      home: SearchScreen(),
+     // home: LoginScreen()
+     home: MyBottomNavigator(),
+     //! làm xog nhớ đổi lại home
     );
   }
 }
