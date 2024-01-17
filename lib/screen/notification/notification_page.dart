412f0806fda43862a893a5ccdcf7a54194e63324
LeNgocBaThong <ngunguoi0909@gmail.com>
2024-01-17 09:31:14 +0700

    Đọc dữ liệu firebase cho màn hình thông báo

--- lib/screen/notification/notification_page.dart
+++ lib/screen/notification/notification_page.dart
@@ -1,8 +1,9 @@
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class NotificationScreen extends StatefulWidget {
   const NotificationScreen({super.key});
-
   @override
   State<NotificationScreen> createState() => _NotificationScreenState();
 }
@@ -22,26 +23,75 @@ class NotificationItem {
 }
 
 class _NotificationScreenState extends State<NotificationScreen> {
+  final notificationn = FirebaseDatabase.instance.ref('notification');
+  
   final List<NotificationItem> notifications = [
     NotificationItem(
       imageUrl:
-          'https://product.hstatic.net/1000362402/product/agct008-2_1_8ed3d98dc1964dcc9c4741a957fa30d8_master_5296d65ea6a5418cb72a868234b425dc_master.jpg',
+          'https://contents.mediadecathlon.com/p2393841/97ec4616ea32d8eaa67ee3c30d2eafd2/p2393841.jpg?format=auto&quality=70&f=650x0',
       title: 'Đã xác nhận',
       content: 'Đơn hàng đã được cửa hàng tiếp nhận và sẻ giao đến cho bạn trong thời gian sớm nhất.',
       date: '01/08/2024',
     ),
+    NotificationItem(
+      imageUrl:
+          'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/63c77c04dc6448548ccbae880189e107_9366/Giay_Galaxy_6_DJen_GW3848_01_standard.jpg',
+      title: 'Đang giao hàng',
+      content: 'Đơn hàng đang được shipper trên đường giao dến bạn.',
+      date: '01/08/2024',
+    ),
+    NotificationItem(
+      imageUrl:
+          'https://assets.adidas.com/images/w_600,f_auto,q_auto/bd43ce71f589498ab6b1aad6009a0e6e_9366/Superstar_Shoes_White_EG4958_07_standard.jpg',
+      title: 'Đã đặt hàng',
+      content: 'Đơn hàng đã được đặt thành công và chờ xác nhận từ cửa hàng.',
+      date: '01/08/2024',
+    ),
+    NotificationItem(
+      imageUrl:
+          'https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/99486859-0ff3-46b4-949b-2d16af2ad421/custom-nike-dunk-high-by-you-shoes.png',
+      title: 'Đã giao hàng thành công',
+      content: 'Đơn hàng đã dến tay của bạn, mong bạn có một dịch vụ mua sắm tốt nhất từ chúng tôi.',
+      date: '01/08/2024',
+    ),
   ];
+  String url = '';
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Color(0xFFD2EDE0),
-        body: ListView.builder(
-          itemCount: notifications.length,
-          itemBuilder: (context, index) {
-            return NotificationCard(notification: notifications[index]);
-          },
-        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: FirebaseAnimatedList(query: notificationn, itemBuilder:(BuildContext context, DataSnapshot snapshot, Animation<double> animation, int index){
+                Map<dynamic, dynamic> values = snapshot.value as Map<dynamic, dynamic> ;
+                String imageUrl = values['imageUrl'];
+                return Card(
+                  color: Colors.green[200],
+                  child: ListTile(
+                    leading: Image.network(
+                      imageUrl,
+                      width: 100.0,
+                      height: 200.0,
+                      fit: BoxFit.cover,
+                      ),
+                    title: Text(snapshot.child('title').value.toString(),style: TextStyle(fontWeight: FontWeight.bold)),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(snapshot.child('description').value.toString(),style: TextStyle(fontSize: 15),),
+                        SizedBox(height: 8.0),
+                        Text(snapshot.child('date').value.toString(),style: TextStyle(fontSize: 12,fontWeight: FontWeight.bold),),
+                      ],
+                    ),
+                  ),
+                );
+              } 
+              ),
+            )
+          ],
+        )
       ),
     );
   }
