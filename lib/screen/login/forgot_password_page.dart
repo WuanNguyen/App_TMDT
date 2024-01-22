ed316bb2b6476beb2a66b48c5f43d025f44eee59
WuanNguyen <nguyenhuanhtk2003@gmail.com>
2024-01-22 16:09:37 +0700

    Thêm sự kiện Forgot_password

--- lib/screen/login/forgot_password_page.dart
+++ lib/screen/login/forgot_password_page.dart
@@ -3,7 +3,9 @@
 import 'dart:async';
 
 import 'package:doan_tmdt/model/bottom_appar.dart';
+import 'package:doan_tmdt/model/err_dialog.dart';
 import 'package:doan_tmdt/screen/login/login_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ForgotPassword extends StatefulWidget {
@@ -19,6 +21,23 @@ class _ForgotPasswordState extends State<ForgotPassword> {
   String TBEmail = "";
   String TBOTP = "Gửi mã";
   bool isOTPSender = false;
+
+  //----
+  Future<void> resetPassword(String email) async {
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+      print('Đã gửi email đặt lại mật khẩu');
+    } catch (e) {
+      print('Lỗi khi gửi email đặt lại mật khẩu: $e');
+      // Xử lý lỗi tại đây
+    }
+  }
+  bool isValidGmail(String input) {
+      // Biểu thức chính quy kiểm tra xem chuỗi là địa chỉ email Gmail hợp lệ hay không
+      final RegExp regex = RegExp(r'^[a-zA-Z0-9._%+-]+@gmail\.com$');
+      return regex.hasMatch(input);
+  }
+  //--------
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -69,48 +88,48 @@ class _ForgotPasswordState extends State<ForgotPassword> {
                         )
                       ),
                       //otp
-                      Row(
-                        children: [
-                          SizedBox(
-                            width: MediaQuery.of(context).size.width/2,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                TextField(
-                                  keyboardType: TextInputType.number,
-                                  maxLength: 6,
-                                  controller: otp,
-                                  decoration: const InputDecoration(label: Text("Mã xác nhận",style:TextStyle(fontWeight: FontWeight.w500))),
-                                ),
-                              ],
-                            )
-                          ),
-                          Padding(padding: EdgeInsets.fromLTRB(22, 0, 0, 0)),
-                          GestureDetector(
-                            onTap: (){
-                              setState(() {
-                                if(email.text.isEmpty){
-                                  TBEmail = "Vui lòng nhập Email";
-                                }
-                                else{
-                                  isOTPSender = true;
-                                  TBEmail ="";
-                                }
-                                if(isOTPSender){
-                                  Timer.periodic(const Duration(seconds: 59),(timer){
-                                    TBOTP = "Chờ ${timer.tick}s";
-                                    if(timer.tick==0){
-                                      TBOTP = "Gửi lại";
-                                      isOTPSender = false;
-                                    }
-                                  });
-                                }
-                              });
-                            },
-                            child: Text(TBOTP,style: TextStyle(color: Color.fromRGBO(14, 52, 201, 1),fontWeight: FontWeight.bold),),
-                          )
-                        ],
-                      ),
+                      // Row(
+                      //   children: [
+                      //     SizedBox(
+                      //       width: MediaQuery.of(context).size.width/2,
+                      //       child: Column(
+                      //         crossAxisAlignment: CrossAxisAlignment.start,
+                      //         children: [
+                      //           TextField(
+                      //             keyboardType: TextInputType.number,
+                      //             maxLength: 6,
+                      //             controller: otp,
+                      //             decoration: const InputDecoration(label: Text("Mã xác nhận",style:TextStyle(fontWeight: FontWeight.w500))),
+                      //           ),
+                      //         ],
+                      //       )
+                      //     ),
+                      //     Padding(padding: EdgeInsets.fromLTRB(22, 0, 0, 0)),
+                      //     GestureDetector(
+                      //       onTap: (){
+                      //         setState(() {
+                      //           if(email.text.isEmpty){
+                      //             TBEmail = "Vui lòng nhập Email";
+                      //           }
+                      //           else{
+                      //             isOTPSender = true;
+                      //             TBEmail ="";
+                      //           }
+                      //           if(isOTPSender){
+                      //             Timer.periodic(const Duration(seconds: 59),(timer){
+                      //               TBOTP = "Chờ ${timer.tick}s";
+                      //               if(timer.tick==0){
+                      //                 TBOTP = "Gửi lại";
+                      //                 isOTPSender = false;
+                      //               }
+                      //             });
+                      //           }
+                      //         });
+                      //       },
+                      //       child: Text(TBOTP,style: TextStyle(color: Color.fromRGBO(14, 52, 201, 1),fontWeight: FontWeight.bold),),
+                      //     )
+                      //   ],
+                      // ),
                     ],
                   ),
                 ),
@@ -125,7 +144,16 @@ class _ForgotPasswordState extends State<ForgotPassword> {
                     FilledButton(
                       onPressed: (){
                         //todo: xác nhận 
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => const MyBottomNavigator()));
+                        if(email.text == '')
+                        {
+                          MsgDialog.ShowDialog(context, 'Sign-In', 'Vui lòng nhập gmail');
+                        }else if(isValidGmail(email.text) == false){
+                           MsgDialog.ShowDialog(context, 'Sign-In', 'Gmail không đúng định dạng. vui lòng nhập lại.!');
+                        }else{
+                           resetPassword(email.text);
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginScreen()));
+                        }
+                       
                       },
                       style: ButtonStyle(
                         backgroundColor: MaterialStateProperty.all(const Color.fromRGBO(120, 120, 120, 1)),
